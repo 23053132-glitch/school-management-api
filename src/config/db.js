a18f3d809e4eee3e040213aad0d821fdc5b976ef
khushboo <23053132@kiit.ac.in>
2026-05-09 19:17:33 +0530

    fix db port and env

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require ('dotenv').config();
 
 const connection = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT  // ✅ this line is missing
 });
 /**
  *tells Node.js try to connect to Mysql database 
